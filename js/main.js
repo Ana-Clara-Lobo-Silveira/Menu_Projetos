b6e1a77f0e09dac10312dcd0c5f7f2d3956318e0
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-02-26 10:51:49 -0300

    ADD: Anotações e constantes.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Lógica
+
+const btnP1 = document.querySelector('#inputP1');
+const btnP2 = document.querySelector('#inputP2');
+const btnP3 = document.querySelector('#inputP3');
+
+btnP1.addEventListener()
