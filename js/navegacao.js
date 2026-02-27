d7bf4b281ce827045eca491d1bce9055bcaafedc
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-02-27 08:27:34 -0300

    ADD: Função de criar botão diretamente no JavaScript.

--- js/navegacao.js
+++ js/navegacao.js
@@ -1,1 +1,17 @@
+// CRIANDO ELEMENTO BOTAO
+ function criarBotaoVoltar(){
+    const btnVoltar = document.createElement('button');
 
+// ESTILO DO BOTAO
+    btnVoltar.innerText = 'Voltar';
+    btnVoltar.style.marginTop = '20px';
+    
+//CLICK  
+    btnVoltar.addEventListener('click', function(){
+        window.location.href = '../index.html'
+    })
+// COLOCAR O ELEMENTO NA PÁGINA (FINAL DA PAGINA)
+    document.body.appendChild(btnVoltar);
+ };
+
+window.onload = criarBotaoVoltar()
